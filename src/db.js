16ebf0b0145dcfffa1153ac77a491ba9b5815035
Balado31 <63737338+Balado31@users.noreply.github.com>
2021-10-19 22:05:42 +0300

    add functions to db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,16 @@
+var send = function (url) {
+  var ajax = new XMLHttpRequest();
+  ajax.open('GET', url, true);
+  ajax.onreadystatechange = function() {
+   if (ajax.readyState == 4) {
+    if (ajax.status == 200) {
+     console.log(ajax.responseText);
+    }
+   }
+  };
+  ajax.send(null);
+}
 
+var getData = function (id) {
+  
+}
