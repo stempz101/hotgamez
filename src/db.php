ba9f2f260662a803555306e134bcf9cd33cf24b8
Balado31 <63737338+Balado31@users.noreply.github.com>
2021-10-20 16:45:15 +0300

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,5 +1,10 @@
 <?php
+   $mysql = new mysql ("hotgamez.mysql.tools", "hotgamez_group", "As-191-cool", "hotgamez_main");
+   $result = $mysql->query("SELECT `name_game` FROM `game`");
+   $games- = $result->fetch_assoc();
+   print_r($games);
+   exit();
 
-echo "Luck";
+   $mysql->close ();
 
 ?>
