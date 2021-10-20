48a482166078c79958cffce2516a089e0d81eac1
Balado31 <63737338+Balado31@users.noreply.github.com>
2021-10-20 12:30:10 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -15,4 +15,17 @@
   
 // }
 
+const con = mysql.createConnection({
+    host: "hotgamez.mysql.tools",
+    user: "hotgamez_group",
+    password: "As-191-cool",
+    database: 'hotgamez_main'
+});
 
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM game", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
