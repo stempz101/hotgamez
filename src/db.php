692bbba5440a97d5db026c44851f46d90b526775
Balado31 <63737338+Balado31@users.noreply.github.com>
2021-10-20 17:15:28 +0300

    Update db.php

--- src/db.php
+++ src/db.php
@@ -2,6 +2,10 @@
    $mysql = new mysql ("hotgamez.mysql.tools", "hotgamez_db", "cavq5Fn5", "hotgamez_db");
    $result = $mysql->query("SELECT `name_game` FROM `game`");
    $games- = $result->fetch_assoc();
+   if(count($games) == 0) {
+    echo "Error #666";
+      exit();
+   }
    print_r($games);
    exit();
 
