1f945bc81adf5eec47c3948d52f328ac863633e2
sainthamster000 <nikolayklyopa000@gmail.com>
2021-10-26 23:39:26 +0300

    тщетные попытки в создании бекграунд блюра

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Home from "./Home";
 import AbobUs from "./AbobUs";
 import "./App.css";
 import * as ReactDom from "@testing-library/react";
+import $ from 'jquery';
 
 
 let loginhidden = true;
 
 function logClick(){
+        $(".container").not("login").addClass("blur");
+
     let page = window.location.href.split('/')[window.location.href.split("/").length - 1];
 
     if(loginhidden === false){
@@ -19,7 +22,8 @@ function logClick(){
         document.getElementById("login").style = "visibility : visible";
         loginhidden = false;
     }
-}
+
+    }
 
 function loginFocusOut(){
     alert("unfocused")
@@ -30,16 +34,15 @@ function focused(){
 }
 
 
-
 const BasicExample = () => (
 
 
 
     <Router>
 
-      <div>
+      <div class = "container">
 
-          <header>
+          <header >
               <a href ="/">
                   <img src="https://imgur.com/zkFCRqh.png" alt="" id={"image"}/>
               </a>
@@ -48,20 +51,28 @@ const BasicExample = () => (
               </form>
               <img src = "https://i.imgur.com/N9Q6FyP.png" alt=""  id = "imagelogo"/>
               <img src = "https://i.imgur.com/vAVPQRO.png" alt=""  id = "searchlogo"/>
-              <input type = "image" src = "https://i.imgur.com/t9wcmFd.png" alt=""  id = "profileicon" onClick={logClick}/>
+              <input type = "image" src = "https://i.imgur.com/t9wcmFd.png" alt=""  id = "profileicon" onClick={logClick} />
 
               <img src="https://i.imgur.com/comJ0Fm.png" alt="wishlist" id = "heart"/>
           </header>
 
-          <div id = "login">
+          <div class = "login" id = "login">
+
+              <div class="wrapper wrapper_blur">
+                  <div class="content"></div>
+              </div>
+              <div class="modal"></div>
+
+
               <div id = "logButtons">
                   <span id = "vhod">Вход</span>
                   <hr id = "upperline"/>
-                  <span id = "mailtext">Электронная почта / номер телефона</span>
+                  <span  id = "mailtext">Электронная почта / номер телефона</span>
                   <input type={"text"} id = "mailinput"/>
-                  <span id = "passtext">Пароль</span>
+                  <span  id = "passtext">Пароль</span>
                   <input type={"text"} id = "passinput"/>
                   <input type={"checkbox"} id = "rememberme"/>
+                  <label htmlFor="checkbox"></label>
                   <span id = "forgotpass">Забыли пароль?</span>
                   <button id = "logingIn">Войти</button>
                   <hr id = "lineUp"/>
