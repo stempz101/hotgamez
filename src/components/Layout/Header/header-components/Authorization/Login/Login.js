5597b8d228dd1cba759e1e95a0aafc1e472a6e1d
sainthamster000 <nikolayklyopa000@gmail.com>
2021-11-17 17:51:54 +0200

    недоделанный логин

--- src/components/Layout/Header/header-components/Authorization/Login/Login.js
+++ src/components/Layout/Header/header-components/Authorization/Login/Login.js
@@ -1,13 +1,22 @@
-import React from 'react'
+﻿import React from 'react'
 
-function Login({setIsOpen, setIsLoginForm}) {
+function Login({ setIsOpen, setIsLoginForm }) {
+   
     return (
         <div>
+            <button class = "closeButton" onClick={() => setIsOpen(false)}>×</button>
             <h2>Login</h2>
-            <button onClick={() => setIsOpen(false)}>Close Modal</button>
-            <button onClick={() => setIsLoginForm(p => !p)}>Change Form</button>
+            <hr></hr>
+            <h3>E-mail</h3>
+            <input class="authinput" type="text" placeholder="Enter an e-mail..." />
+            <h3>Password</h3>
+            <input class = "authinput" type="text" placeholder="Enter a password..." />
+            <button class="authbutton">Log in</button>
+            <button class="authbutton"onClick={() => setIsLoginForm(p => !p)}>Registration</button>
+
         </div>
     )
 }
+
 
 export default Login
