9cbcb07b63bef607b20ec91263d3349abb47797f
Balado <alina.vytnova@gmail.com>
2021-12-01 01:00:14 +0200

    api избранное

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,5 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'login']);
 Route::apiResources([
-    'games' => GameController::class
+    'games' => GameController::class,
+    'favorites' => FavoriteController::class
 ]);
