971548a73e2c9c513bb6e4273a098f89fa3912fa
craftaXLB <craftalion@live.ru>
2021-11-30 22:31:02 +0200

    Профиль

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,19 +1,67 @@
 import React from "react";
 import "./Profile.css";
 import Layout from '../../components/Layout/Layout'
+import axios from "axios";
 
 
 function Profile(){
 
+    const [user, setUser] = React.useState({})
+    React.useEffect(()=>{
+        axios.get("http://127.0.0.1:8000/api/user", {withCredentials : true, headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
+            .then(res => {
+                setUser(res.data)
+            })
+    })
 
     let lul = "sda"
     return (
         <Layout>
-            <div className={"profile_data_box"}>
-                <textarea placeholder={"Имя"} className={"name"}></textarea>
-                <textarea placeholder={"mail@gmail.com"} className={"mail"}></textarea>
-                <textarea placeholder={"Пароль"} className={"password"}></textarea>
+            <div className={"profile_data_box_up"}>
+                <span className={"profText"}>Настройки профиля</span>
+                <div className={"inputRow"}>
+                    <textarea placeholder={"Имя"} className={"name"}></textarea>
+                    <span className={"changeButt"}>  Изменить</span>
+                </div>
+                <br/>
+                <div className={"inputRow"}>
+                    <textarea placeholder={"mail@gmail.com"} className={"mail"}></textarea>
+                    <span className={"changeButt"}>  Изменить</span>
+                </div>
+                <br/>
+                <div className={"inputRow"}>
+                    <textarea placeholder={"Пароль"} className={"password"}></textarea>
+                    <span className={"changeButt"}>  Изменить</span>
+                </div>
+                <br/>
+                <br/>
                 <span className={"profile_data_bottom_text"}>Для входа вы можете использовать ваши почту и пароль</span>
+            </div>
+
+            <div className={"profile_data_box_down"}>
+                <div className={"row"}>
+                    <span>Информация</span>
+                </div>
+                <br/>
+                <div className={"row"}>
+                    <span>Email : </span>
+                    <span>{user.email}</span>
+                </div>
+                <br/>
+                <div className={"row"}>
+                    <span>Никнейм : </span>
+                    <span>{user.username}</span>
+                </div>
+                <br/>
+                <div className={"row"}>
+                    <span>Количество игр в списке желаемого : </span>
+                    <span>0</span>
+                </div>
+                <br/>
+                <div className={"row"}>
+                    <span>Количество отправленых пожеланий в улучшении сайта : </span>
+                    <span>0</span>
+                </div>
             </div>
         </Layout>
     )
