040f46016d134beea9433e48326f2471be165bda
Bogdan Bazelyuk <bazelyuk.b@ukr.net>
2021-12-02 01:00:39 +0200

    update password/username/email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,14 @@ use App\Exceptions\ErrorCode;
 use App\Exceptions\LoginException;
 use App\Exceptions\UpdateUserException;
 use App\Models\User;
-use http\Env\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @method validate(Request $request, string[] $array)
  */
-class UserController
+class UserController extends Controller
 {
     public function getUser()
     {
@@ -37,8 +38,47 @@ class UserController
         }
         $user = Auth::user();
         $user->username = $request->input('username');
+        $user->save();
+        return $this->successResponse($user);
+    }
 
+    public function updatePassword(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'password' => 'required'
+            ]);
+        } catch (\Exception $e){
+            throw new LoginException('Password cannot be empty!', ErrorCode::VALIDATION_ERROR);
+        }
+        $newPassword = Hash::make($request->input('password'));
+        $user = Auth::user();
+        $user->password = $newPassword;
+        $user->save();
+        return $this->successResponse($user);
+    }
 
+    public function updateEmail(Request $request)
+    {
+        $newEmail = $request->input('email');
+        if ($newEmail == ""){
+            throw new LoginException('Email cannot be empty!', ErrorCode::VALIDATION_ERROR);
+        }
+        try {
+            $this->validate($request, [
+                'email' => 'email'
+            ]);
+        } catch (\Exception $e){
+            throw new LoginException('Validation error!', ErrorCode::VALIDATION_ERROR);
+        }
+        $user = User::where('email', $newEmail)->first();
+        if ($user) {
+            throw new UpdateUserException('Email is taken!!', ErrorCode::THIS_EMAIL_IS_ALREADY_TAKEN);
+        }
+        $user = Auth::user();
+        $user->email = $newEmail;
+        $user->save();
+        return $this->successResponse($user);
     }
 
 }
