707376aa952db2739bfa8b7267db24589a7ea785
craftaXLB <craftalion@live.ru>
2021-12-02 03:23:34 +0200

    кнопки на главной и в профайле

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -16,27 +16,59 @@ function Profile(){
 
     return (
         <Layout>
-            <button  className="favorite">Добавить</button>
-
             <div className={"profile_data_box_down2"}>
             </div>
             <div>
             <div className={"profile_data_box_up"}>
                 <span className={"profText"}>Настройки профиля</span>
                 <div className={"inputRow"}>
-                    <textarea placeholder={"Имя"} className={"input"}></textarea>
-                    <span className={"changeButt"}>  Изменить</span>
+                    <textarea placeholder={"Имя"} id ={"changeButtUs"} className={"input"}></textarea>
+                    <span className={"changeButt"} onClick={() =>{
+                        axios.put(`http://127.0.0.1:8000/api/user/username?username=${document.getElementById("changeButtUs").value}`, {} ,{withCredentials : true, headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
+                            .then(res => {
+                                alert("Username changed")
+                            })
+                            .catch(err =>{
+                                alert(JSON.stringify(err.response));
+                            })
+                    }}>  Изменить</span>
                 </div>
                 <br/>
                 <div className={"inputRow"}>
-                    <textarea placeholder={"mail@gmail.com"} className={"input"}></textarea>
-                    <span className={"changeButt"}>  Изменить</span>
+                    <textarea placeholder={"mail@gmail.com"} id ={"changeButtEm"} className={"input"}></textarea>
+                    <span className={"changeButt"}  onClick={() =>{
+                        axios.put(`http://127.0.0.1:8000/api/user/email?email=${document.getElementById("changeButtEm").value}`, {} ,{withCredentials : true, headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
+                            .then(res => {
+                                alert("Email changed")
+                            })
+                            .catch(err =>{
+                                alert(JSON.stringify(err.response));
+                            })
+                    }}>  Изменить</span>
                 </div>
                 <br/>
                 <div className={"inputRow"}>
-                    <textarea placeholder={"Пароль"} className={"input"}></textarea>
-                    <span className={"changeButt"}>  Изменить</span>
+                    <textarea placeholder={"Пароль"} id ={"changeButtPass"} className={"input"}></textarea>
+                    <span className={"changeButt"} onClick={() =>{
+                        axios.put(`http://127.0.0.1:8000/api/user/password?password=${document.getElementById("changeButtPass").value}`, {} ,{withCredentials : true, headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
+                        .then(res => {
+                        alert("Password changed")
+                    })
+                        .catch(err =>{
+                        alert(JSON.stringify(err.response));
+                    })
+                    }}>  Изменить</span>
                 </div>
+                <button  className="favorite" onClick={() => {
+                    axios.post(`http://127.0.0.1:8000/api/logout`, {} ,{withCredentials : true, headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
+                        .then(res => {
+                            alert(JSON.stringify(res))
+                            alert("Logged out successfully")
+                        })
+                        .catch(err =>{
+                            alert(JSON.stringify(err.response));
+                        })
+                }}>Выйти</button>
 
                 <br/>
                 <br/>
